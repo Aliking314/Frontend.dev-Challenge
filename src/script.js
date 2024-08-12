d1ae3cd7cb87f22590afae74e95a312c9751cf5a
Aliking314 <appenpvtltd@gmail.com>
2024-08-12 11:43:38 +0500

    updated

--- src/script.js
+++ src/script.js
@@ -3,7 +3,9 @@ let button = document.querySelector('button')
 let checkBox =document.querySelector('#checkBox')
 let para =document.querySelector('#pra') 
 let main =document.querySelector('#main') 
+let heading = document.querySelector('h2')
 let ThemeToggler = document.querySelector('.fa-solid')
+
 
 let theme = false;
 
@@ -25,9 +27,7 @@ function validateEmail(email) {
     return regex.test(email);
   }
 
-  function validateCheckbox() {
-    checkBox.value.checked = true;
-  }
+
 
 button.addEventListener('click',(e)=>{
     e.preventDefault()
@@ -36,10 +36,25 @@ button.addEventListener('click',(e)=>{
       e.preventDefault(); 
       alert('Please enter a valid email address')
     } else {
-     alert('Thank you for subscribing')
-     location.reload()
-    }
-    validateCheckbox()
-})
+     heading.innerText="Thanks for Subscribing"
+     heading.style.textAlign='center'
+     heading.style.marginTop='20%'
+     emailInput.value =''
+     checkBox.checked = false
+     para.style.display = 'none'
+     main.style.display = 'none'
+     button.style.display = 'none'
+     document.body.style.backgroundColor = '#222'
+     document.body.style.color = 'white'
 
+     setTimeout(()=>{
+       heading.innerText='Redirecting...'
+     },3000)
+     setTimeout(()=>{
+        location.reload()
+     },4000)
+    }
+
+})
+ 
 
